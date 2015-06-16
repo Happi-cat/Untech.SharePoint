e7b15e68f0ce1547ace2711fe5ef85be1bb06324
Siarhei Svirski <zversergey@tut.by>
2015-06-16 11:26:30 +0300

    BooleanFieldConverter

---
+++ Untech.SharePoint.Core/Data/Converters/BooleanFieldConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.SharePoint;
+
+namespace Untech.SharePoint.Core.Data.Converters
+{
+	[SPFieldConverter("Boolean")]
+	public class BooleanFieldConverter : IFieldConverter
+	{
+		public object FromSpValue(object value, SPField field, Type propertyType)
+		{
+			Guard.ThrowIfNot<SPFieldBoolean>(field, "This Field Converter doesn't support that SPField type");
+
+			if (propertyType.IsGenericType && propertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+				return (bool?)value;
+
+			return (bool?)value ?? false;
+		}
+
+		public object ToSpValue(object value, SPField field, Type propertyType)
+		{
+			Guard.ThrowIfNot<SPFieldBoolean>(field, "This Field Converter doesn't support that SPField type");
+
+			return (bool?)value;
+		}
+	}
+}
