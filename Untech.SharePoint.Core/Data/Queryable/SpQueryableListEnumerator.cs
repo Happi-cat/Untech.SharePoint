be978243964d7a7a4c65268ad86b9c433f664b8b
Siarhei Svirski <zversergey@tut.by>
2015-07-07 11:23:56 +0300

    enumerator

---
+++ Untech.SharePoint.Core/Data/Queryable/SpQueryableListEnumerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Untech.SharePoint.Core.Reflection;
+
+namespace Untech.SharePoint.Core.Data.Queryable
+{
+	internal class SpQueryableListEnumerator<TElement> : IEnumerator<TElement> where TElement: new()
+	{
+		internal SpQueryableListEnumerator(IEnumerator<SPListItem> spListItemIterator)
+		{
+			Guard.ThrowIfArgumentNull(spListItemIterator, "spListItemIterator");
+
+			SPListItemIterator = spListItemIterator;
+			CurrentReady = false;
+		}
+
+		public IEnumerator<SPListItem> SPListItemIterator { get; set; }
+		private bool CurrentReady { get; set; }
+		private TElement CurrenElement { get; set; }
+
+		public TElement Current
+		{
+			get
+			{
+				if (!CurrentReady)
+				{
+					CurrenElement = new TElement();
+
+					SpModelMapper.Map<TElement>(SPListItemIterator.Current, CurrenElement);
+
+					CurrentReady = true;
+				}
+				return CurrenElement;
+			}
+		}
+
+		public void Dispose()
+		{
+			SPListItemIterator.Dispose();
+		}
+
+		object System.Collections.IEnumerator.Current
+		{
+			get { return Current; }
+		}
+
+		public bool MoveNext()
+		{
+			CurrentReady = false;
+
+			return SPListItemIterator.MoveNext();
+		}
+
+		public void Reset()
+		{
+			CurrentReady = false;
+
+			SPListItemIterator.Reset();
+		}
+	}
+}
