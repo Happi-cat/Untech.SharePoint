81e212aadd4532cee515f19bc2422750b6e4fe60
Siarhei Svirski <zversergey@tut.by>
2015-07-21 17:55:48 +0300

    predicate modifier

--- Untech.SharePoint.Core/Caml/Modifiers/PredicateModifier.cs
+++ Untech.SharePoint.Core/Caml/Modifiers/PredicateModifier.cs
@@ -4,94 +4,43 @@ using Untech.SharePoint.Core.Extensions;
 
 namespace Untech.SharePoint.Core.Caml.Modifiers
 {
-	internal class PredicateModifier : ExpressionVisitor
+	internal class EqualityAdder : ExpressionVisitor
 	{
-		private readonly IReadOnlyDictionary<ExpressionType, ExpressionType> _binaryInverseMap = new Dictionary<ExpressionType, ExpressionType>
-		{
-			{ExpressionType.AndAlso, ExpressionType.OrElse},
-			{ExpressionType.OrElse, ExpressionType.AndAlso}
-		};
-
-		private readonly IReadOnlyDictionary<ExpressionType, ExpressionType> _compareInverseMap = new Dictionary<ExpressionType, ExpressionType>
-		{
-			{ExpressionType.Equal, ExpressionType.NotEqual},
-			{ExpressionType.LessThan, ExpressionType.GreaterThanOrEqual},
-			{ExpressionType.LessThanOrEqual, ExpressionType.GreaterThan},
-			{ExpressionType.GreaterThan, ExpressionType.LessThanOrEqual},
-			{ExpressionType.GreaterThanOrEqual, ExpressionType.LessThan}
-		};
-
 		protected override Expression VisitBinary(BinaryExpression node)
 		{
-			if (node.NodeType == ExpressionType.AndAlso || 
-				node.NodeType == ExpressionType.OrElse)
+			if (node.NodeType != ExpressionType.AndAlso && node.NodeType != ExpressionType.OrElse)
 			{
-				var left = node.Left.StripQuotes();
-				var right = node.Right.StripQuotes();
-				var predicateUpdated = false;
-
-				if (CanMakeEqualTrueFalse(left))
-				{
-					left = MakeEqualTrueFalse(left);
-					predicateUpdated = true;
-				}
-
-				if (CanMakeEqualTrueFalse(right))
-				{
-					right = MakeEqualTrueFalse(right);
-					predicateUpdated = true;
-				}
-
-				if (predicateUpdated)
-				{
-					return base.VisitBinary(node.Update(left, VisitAndConvert(node.Conversion, "VisitBinary"), right));
-				}
+				return base.VisitBinary(node);
 			}
 
-			return base.VisitBinary(node);
-		}
+			var left = node.Left.StripQuotes();
+			var right = node.Right.StripQuotes();
 
-		protected override Expression VisitUnary(UnaryExpression node)
-		{
-			if (node.NodeType == ExpressionType.Not)
+			if (CanMakeEqualTrueFalse(left))
 			{
-				var innerOperand = node.Operand.StripQuotes();
-
-				if (_binaryInverseMap.ContainsKey(innerOperand.NodeType))
-				{
-					var binary = (BinaryExpression)innerOperand;
-
-					var newBinary = Expression.MakeBinary(_binaryInverseMap[innerOperand.NodeType], 
-						Expression.Not(binary.Left), Expression.Not(binary.Right),
-						binary.IsLiftedToNull, binary.Method, binary.Conversion);
-
-					return Visit(newBinary);
-				}
-				if (_compareInverseMap.ContainsKey(innerOperand.NodeType))
-				{
-					var binary = (BinaryExpression)innerOperand;
-
-					var newBinary = Expression.MakeBinary(_compareInverseMap[innerOperand.NodeType], binary.Left, binary.Right,
-						binary.IsLiftedToNull, binary.Method, binary.Conversion);
-
-					return Visit(newBinary);
-				}
+				left = MakeEqualTrueFalse(left);
 			}
-			return base.VisitUnary(node);
+
+			if (CanMakeEqualTrueFalse(right))
+			{
+				right = MakeEqualTrueFalse(right);
+			}
+
+			return base.VisitBinary(node.Update(left, VisitAndConvert(node.Conversion, "VisitBinary"), right));
 		}
 
 		protected override Expression VisitMethodCall(MethodCallExpression node)
 		{
 			if (node.Method.Name == "Where")
 			{
-				var lambda = (LambdaExpression) node.Arguments[1].StripQuotes();
+				var lambda = (LambdaExpression)node.Arguments[1].StripQuotes();
 				var predicate = lambda.Body.StripQuotes();
 
 				if (CanMakeEqualTrueFalse(predicate))
 				{
 					var newLambda = Expression.Lambda(MakeEqualTrueFalse(predicate), lambda.Parameters);
 
-					return base.VisitMethodCall(node.Update(null, new[] {node.Arguments[0], newLambda}));
+					return base.VisitMethodCall(node.Update(null, new[] { node.Arguments[0], newLambda }));
 				}
 			}
 
@@ -103,7 +52,7 @@ namespace Untech.SharePoint.Core.Caml.Modifiers
 		private static bool CanMakeEqualTrueFalse(Expression node)
 		{
 			return node.NodeType == ExpressionType.MemberAccess ||
-			       node.NodeType == ExpressionType.Not;
+				   node.NodeType == ExpressionType.Not;
 		}
 
 		private static Expression MakeEqualTrueFalse(Expression node)
@@ -111,25 +60,77 @@ namespace Untech.SharePoint.Core.Caml.Modifiers
 			switch (node.NodeType)
 			{
 				case ExpressionType.MemberAccess:
-					return MakeEqualTrueFalse((MemberExpression) node, true);
+					return MakeEqualTrueFalse(node, true);
 				case ExpressionType.Not:
-					var notExpression = (UnaryExpression) node;
-					var innerExpression = notExpression.Operand.StripQuotes();
-					if (innerExpression.NodeType == ExpressionType.MemberAccess)
+					var notExpression = (UnaryExpression)node;
+					var notOperand = notExpression.Operand.StripQuotes();
+					if (notOperand.NodeType == ExpressionType.MemberAccess)
 					{
-						return MakeEqualTrueFalse((MemberExpression) innerExpression, false);
+						return MakeEqualTrueFalse(notOperand, false);
 					}
 					break;
 			}
 			return node;
 		}
 
-		private static Expression MakeEqualTrueFalse(MemberExpression node, bool equalTruth)
+		private static Expression MakeEqualTrueFalse(Expression node, bool equalTruth)
 		{
 			return Expression.Equal(node, Expression.Constant(equalTruth));
 		}
 
 		#endregion
+	}
 
+	internal class PredicateModifier : ExpressionVisitor
+	{
+		private static readonly IReadOnlyDictionary<ExpressionType, ExpressionType> _binaryInverseMap = new Dictionary<ExpressionType, ExpressionType>
+		{
+			{ExpressionType.AndAlso, ExpressionType.OrElse},
+			{ExpressionType.OrElse, ExpressionType.AndAlso}
+		};
+
+		private static readonly IReadOnlyDictionary<ExpressionType, ExpressionType> _compareInverseMap = new Dictionary<ExpressionType, ExpressionType>
+		{
+			{ExpressionType.Equal, ExpressionType.NotEqual},
+			{ExpressionType.LessThan, ExpressionType.GreaterThanOrEqual},
+			{ExpressionType.LessThanOrEqual, ExpressionType.GreaterThan},
+			{ExpressionType.GreaterThan, ExpressionType.LessThanOrEqual},
+			{ExpressionType.GreaterThanOrEqual, ExpressionType.LessThan}
+		};
+
+		protected override Expression VisitUnary(UnaryExpression node)
+		{
+			if (node.NodeType != ExpressionType.Not)
+			{
+				return base.VisitUnary(node);
+			}
+
+			var notOperand = node.Operand.StripQuotes();
+
+			if (_binaryInverseMap.ContainsKey(notOperand.NodeType))
+			{
+				var binaryNode = (BinaryExpression)notOperand;
+
+				var newBinaryNode = Expression.MakeBinary(_binaryInverseMap[notOperand.NodeType],
+					Expression.Not(binaryNode.Left), Expression.Not(binaryNode.Right),
+					binaryNode.IsLiftedToNull, binaryNode.Method, binaryNode.Conversion);
+
+				return Visit(newBinaryNode);
+			}
+			if (_compareInverseMap.ContainsKey(notOperand.NodeType))
+			{
+				var binaryNode = (BinaryExpression)notOperand;
+
+				var newBinaryNode = Expression.MakeBinary(_compareInverseMap[notOperand.NodeType], binaryNode.Left, binaryNode.Right,
+					binaryNode.IsLiftedToNull, binaryNode.Method, binaryNode.Conversion);
+
+				return Visit(newBinaryNode);
+			}
+			if (notOperand.NodeType == ExpressionType.Not)
+			{
+				return Visit(((UnaryExpression)notOperand).Operand);
+			}
+			return base.VisitUnary(node);
+		}
 	}
 }
