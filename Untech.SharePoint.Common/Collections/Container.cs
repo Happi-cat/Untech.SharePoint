75a5faba100cbcd746fd7ccdf936036c01728bf0
Siarhei Svirski <zversergey@tut.by>
2015-09-09 17:19:43 +0300

    container and config

---
+++ Untech.SharePoint.Common/Collections/Container.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Untech.SharePoint.Common.Collections;
+{
+	public class Container<TKey, TObject>
+	{
+		public Dictionary<TKey, TObject> _registeredObjects = new Dictionary<TKey, TObject>();
+
+		public void Register(TKey key, TObject obj)
+		{
+			_registeredObjects.Add(key, obj);
+		}
+
+		public TObject Resolve(TKey key)
+		{
+			return _registeredObjects[key];
+		}
+
+		public bool IsRegistered(TKey key)
+		{
+			return _registeredObjects.ContainsKey(key);
+		}
+	}
+}
--- Untech.SharePoint.Common/Configuration/Configuration.cs
+++ Untech.SharePoint.Common/Configuration/Configuration.cs
@@ -1,7 +1,29 @@
-﻿namespace Untech.SharePoint.Common.Configuration
+﻿using Untech.SharePoint.Common.MetaModels.Providers;
+using Untech.SharePoint.Common.Collections;
+using System;
+
+namespace Untech.SharePoint.Common.Configuration
 {
+	public class MappingConfiguration
+	{
+		public IMetaContextProvider AnnotatedMapping<T>()
+		{
+			return new AnnotationMapping.AnnotatedContextMapping<T>()
+		}
+	}
+
 	public class Configuration
 	{
-		 
+		private Container<Type, IMetaContextProvider> _metaContextProviders = new Container<Type,IMetaContextProvider>();
+
+		public void RegisterMapping<TContext>(Func<MappingConfiguration, IMetaContextProvider> contextProviderBuidler)
+		{
+			RegisterMapping<TContext>(contextProviderBuidler);
+		}
+
+		public void RegisterMapping<TContext>(IMetaContextProvider contextProvider)
+		{
+			_metaContextProviders.Register(typeof(TContext), contextProvider);
+		}
 	}
 }
