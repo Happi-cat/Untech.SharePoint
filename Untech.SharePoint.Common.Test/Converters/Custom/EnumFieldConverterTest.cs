ed1ed15beecbac98cb88801b2b1259ec3f38362c
Siarhei Svirski <zversergey@tut.by>
2015-12-03 12:43:27 +0300

    enum field converter

--- Untech.SharePoint.Common.Test/Converters/Custom/EnumFieldConverterTest.cs
+++ Untech.SharePoint.Common.Test/Converters/Custom/EnumFieldConverterTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Untech.SharePoint.Common.Converters;
 using Untech.SharePoint.Common.Converters.Custom;
@@ -23,6 +24,36 @@ namespace Untech.SharePoint.Common.Test.Converters.Custom
 				.CanConvertToCaml(TestEnum.Option2, "Option 2");
 		}
 
+		[TestMethod]
+		public void CanConvertNullableTestEnum()
+		{
+			Given<TestEnum?>()
+				.CanConvertFromSp("Default", TestEnum.Default)
+				.CanConvertFromSp("Option1", TestEnum.Option1)
+				.CanConvertFromSp("Option2", TestEnum.Option2)
+				.CanConvertFromSp("Option 2", TestEnum.Option2)
+				.CanConvertFromSp(null, null)
+				.CanConvertToSp(TestEnum.Default, "Default")
+				.CanConvertToSp(TestEnum.Option2, "Option 2")
+				.CanConvertToSp(null, null)
+				.CanConvertToCaml(TestEnum.Option1, "Option1")
+				.CanConvertToCaml(TestEnum.Option2, "Option 2")
+				.CanConvertToCaml(null, "");
+		}
+
+		[TestMethod]
+		public void NotSupportEnumWithoutDefault()
+		{
+			CustomAssert.Throw<ArgumentException>(() => Given<NoDefaultEnum>());
+		}
+
+
+		[TestMethod]
+		public void NotSupportInt()
+		{
+			CustomAssert.Throw<ArgumentException>(() => Given<int>());
+		}
+
 		protected override IFieldConverter GetConverter()
 		{
 			return new EnumFieldConverter();
@@ -31,6 +62,14 @@ namespace Untech.SharePoint.Common.Test.Converters.Custom
 		public enum TestEnum
 		{
 			Default = 0,
+			Option1,
+			[EnumMember(Value = "Option 2")]
+			Option2
+		}
+
+		public enum NoDefaultEnum
+		{
+			Default = 1,
 			Option1,
 			[EnumMember(Value = "Option 2")]
 			Option2
