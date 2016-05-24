287efa8b8c09890bc0d8c1dad5520e964ac27db4
Svirski Siarhei <zversergey@tut.by>
2016-05-24 23:04:57 +0300

    missing files

---
+++ Untech.SharePoint.Common.Test/Tools/QueryTests/TestQuery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Untech.SharePoint.Common.Test.Tools.QueryTests
+{
+	public class TestQuery<T, TResult> : ITestQueryAcceptor<T>
+	{
+		public TestQuery(Func<IQueryable<T>, object> query, IEqualityComparer<TResult> comparer = null, Type exception = null, string caml = null)
+		{
+			Query = query;
+			Comparer = comparer ?? EqualityComparer<TResult>.Default;
+			Exception = exception;
+			Caml = caml;
+		}
+
+		public Func<IQueryable<T>, object> Query { get; private set; }
+
+		public IEqualityComparer<TResult> Comparer { get; private set; }
+
+		public Type Exception { get; private set; }
+
+		public string Caml { get; private set; }
+
+		public void Accept(ITestQueryExcecutor<T> executor)
+		{
+			executor.Visit(Query, Comparer, Exception, Caml);
+		}
+	}
+}
